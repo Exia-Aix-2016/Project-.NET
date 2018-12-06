36f54a38e182a2e4e58bbc362113325fbdd0a57b
VAUBOURG Mandel <mandel.vaubourg@viacesi.fr>
2018-12-06 17:29:47 +0100

    Add SimulationController #13

---
+++ Dinner/SimulationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dinner
+{
+    public class SimulationController : Model.ISimulation
+    {
+        public void Resume()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SlowDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SpeedUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
